88dfee409a13f1d81d3255ee8fc69cf02f131d94
Maulana Imamul Khaq <mm930199@gmail.com>
2022-10-27 23:13:21 +0700

    add alternative port

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -15,7 +15,7 @@ import * as dotenv from 'dotenv'
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 8080;
 // const dbURI = process.env.NX_DB_URI;
 const dbURI = 'mongodb://tweet:circletweet@ac-ffppojj-shard-00-00.7hfkjqd.mongodb.net:27017,ac-ffppojj-shard-00-01.7hfkjqd.mongodb.net:27017,ac-ffppojj-shard-00-02.7hfkjqd.mongodb.net:27017/main-dev?ssl=true&replicaSet=atlas-x6t3cc-shard-0&authSource=admin&retryWrites=true&w=majority';
 const server = new Server(port, app);
